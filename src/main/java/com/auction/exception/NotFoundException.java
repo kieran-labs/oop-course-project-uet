654511bea5bf3fa77a69f309343850fa966c6c7a
HumaNormal <buingocphuhung@gmail.com>
2026-05-06 01:37:55 +0700

    docs(NotFoundException): add Javadoc with usage example

--- src/main/java/com/auction/exception/NotFoundException.java
+++ src/main/java/com/auction/exception/NotFoundException.java
@@ -1,13 +1,37 @@
 package com.auction.exception;
 
+/**
+ * Thrown when a requested entity (User, Item, Auction, Bid, ...) cannot be found
+ * in the underlying data source.
+ *
+ * <p>Typical usage:
+ * <pre>{@code
+ * Auction auction = auctionDao.findById(id)
+ *     .orElseThrow(() -> new NotFoundException("Auction not found with id: " + id));
+ * }</pre>
+ *
+ * @see AuctionException
+ */
 public class NotFoundException extends AuctionException {
 
     private static final long serialVersionUID = 1L;
-  public NotFoundException(String message) {
-    super(message);
-  }
 
-  public NotFoundException(String message, Throwable cause) {
-    super(message, cause);
-  }
+    /**
+     * Constructs a new NotFoundException with the specified message.
+     *
+     * @param message description of which entity was not found
+     */
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new NotFoundException with the specified message and cause.
+     *
+     * @param message description of which entity was not found
+     * @param cause   the underlying exception (e.g., SQLException)
+     */
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
